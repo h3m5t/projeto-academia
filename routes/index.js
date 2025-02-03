0a34c495690f032564e92b6c8e7aec75f1c63dc9
h3m5t <henriquee.trajanoo@gmail.com>
2025-02-03 15:08:48 -0300

    mudança dia 03/02
    - Adicionei uma rota para listar os funcionários
    - Add também uma interface para listar os autores
    - Problema em relação a distribuição dos nomes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+let db = require('../utils/db');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Bem Vindo' });
 });
+
+
+/* Rota Funcionario */
+router.get('/funcionarios/listar',function(req,res){
+  let cmd = 'SELECT mat_funcionario AS Matricula, nome_func AS Nome, tel_func AS contato,'
+  cmd +=     'dt_nascimento AS Aniversário, cpf_func AS Cpf '
+  cmd +=     'FROM tbfuncionario;'
+  db.query(cmd, [], function(erro, listagem){
+    if(erro){
+      res.send(erro);
+    }
+    res.render('funcionario-lista', {resultado: listagem});
+  });
+});
+
+
 
 module.exports = router;
