7f12217120757024e08da3e78946cffc49033417
h3m5t <henriquee.trajanoo@gmail.com>
2025-02-05 17:58:19 -0300

    dia 05/02/25
    add rota para adicionar funcionario, mas nao testei. Chatgpt fez o codigo para mim, nn sabia a estrutura.

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ router.get('/', function(req, res, next) {
 });
 
 
-/* Rota Funcionario */
+/* Rota listar funcionários*/
 router.get('/funcionarios/listar',function(req,res){
   let cmd = 'SELECT mat_funcionario AS Matricula, nome_func AS Nome, tel_func AS contato,'
   cmd +=     'dt_nascimento AS Aniversário, cpf_func AS Cpf '
@@ -23,4 +23,26 @@ router.get('/funcionarios/listar',function(req,res){
 
 
 
+
+
+/* Rota para add Funcionario */
+router.post('/funcionarios/add', function(req, res) {
+  let { nome, telefone, cpf, id_cargo } = req.body;
+
+  let cmd = `INSERT INTO tbfuncionario (nome_func, tel_func, cpf_func, id_cargo) 
+             VALUES (?, ?, ?, ?)`;
+
+  db.query(cmd, [nome, telefone, cpf, id_cargo], function(erro, resultado) {
+      if (erro) {
+          res.render('funcionario-add', { mensagem: 'Erro ao cadastrar funcionário!', erro });
+          return;
+      }
+      res.render('funcionario-add', { mensagem: 'Funcionário cadastrado com sucesso!' });
+  });
+});
+
+
 module.exports = router;
+
+
+/* como add */
