acf8cc522aa7dae29a8c68cca727298e9f8ce8c8
h3m5t <henriquee.trajanoo@gmail.com>
2025-02-11 14:28:21 -0300

    add funcionando, editar aparecendo as info, menos dt_nasc

--- routes/funcionario.js
+++ routes/funcionario.js
@@ -33,11 +33,11 @@ router.post('/add', function(req, res) {
 
   db.query(cmd, [nome, cpf, telefone, nascimento, cargo], function(erro, resultado) {
       if (erro) {
-          console.error('Erro ao adicionar funcionário:', erro);
           return res.status(500).send('Erro ao adicionar funcionário');
       }
 
       res.status(200).send('Funcionário cadastrado com sucesso!');
+      res.render('funcionario-lista');
   });
 });
 
@@ -47,7 +47,7 @@ router.post('/add', function(req, res) {
 
 /* Rota para editar Funcionario */
 router.get('/editar/:mat',function(req,res){
-  let mat = req.params.mat
+  let mat = req.params.mat; 
 
   let cmd = 'SELECT mat_funcionario AS Matricula, nome_func AS Nome, tel_func AS contato,'
   cmd +=     'DATE_FORMAT(dt_nascimento,"%d/%m/%Y") AS Aniversario, cpf_func AS Cpf '
@@ -59,7 +59,26 @@ router.get('/editar/:mat',function(req,res){
     res.render('funcionario-add', {resultado: listagem[0]});
   });
 });
-  
+
+router.put('/editar/:mat', function(req, res) {
+  let mat = req.params.mat
+  let nome = req.body.nome;
+  let cpf = req.body.cpf;
+  let telefone = req.body.telefone;
+  let nascimento = req.body.nascimento;
+  let cargo = req.body.cargo;
+
+  let cmd = 'UPDATE tbfuncionario SET nome_func = ?, tel_func = ?, cpf_func = ?, dt_nascimento = ?, id_cargo= ? WHERE mat_funcionario = ? ;';
+
+  db.query(cmd, [nome, cpf, telefone, nascimento, cargo], function(erro, resultado) {
+      if (erro) {
+          return res.status(500).send('Erro ao adicionar funcionário');
+      }
+
+      res.redirect(303, '/funcionario/listar');
+    
+  });
+});
 
 module.exports = router;
 
