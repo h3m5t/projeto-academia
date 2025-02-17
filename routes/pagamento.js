4462094a0b744f4c4b62642a3bdcd84213f4e316
h3m5t <henriquee.trajanoo@gmail.com>
2025-02-17 09:52:20 -0300

    começo de pagamento e horarios

--- routes/pagamento.js
+++ routes/pagamento.js
@@ -3,13 +3,39 @@ var router = express.Router();
 let db = require('../utils/db');
 
 /* aqui será destinado para realizar os pagamentos dos clientes */
-
+router.get('/add',function(req,res){
+    res.render('funcionario-add',{resultado:{}});
+ })
+ 
+ router.post('/add', function(req, res) {
+   
+ 
+   let cmd = 'INSERT INTO tbfuncionario (nome_func, tel_func, cpf_func, dt_nascimento, id_cargo) VALUES ( ?, ?, ?, ?, ?);';
+ 
+   db.query(cmd, [nome, cpf, telefone, nascimento, cargo], function(erro, resultado) {
+       if (erro) {
+           return res.status(500).send('Erro ao adicionar funcionário');
+       }
+ 
+       res.status(200).send('Funcionário cadastrado com sucesso!');
+       res.render('funcionario-lista');
+   });
+ });
 
 /* rota p/ listar pagamentos */ 
+router.get('/listar',function(req,res){
+    let cmd = 'SELECT DATE_FORMAT(dt_pagamento,"%d/%m/%Y") AS dt_pag, id_cliente AS mat, id_pagamento AS pagamento FROM tbregistropagamento;'
+
+    db.query(cmd, [], function(erro, listagem){
+      if(erro){
+        res.send(erro);
+      }
+      res.json(listagem)
+    });
+  });
 
 
 
-/* rota p/  add pagamentos */ 
 
 
 module.exports = router;
--- routes/registros.js
+++ routes/registros.js
@@ -4,6 +4,10 @@ let db = require('../utils/db');
 
 /* Ideia: colocar o registro de treino dos clientes aq numa rota */
 
+
+
 /* Rota para listar registro de pagamentos por mês/semana/ano talvez todoskkkkk*/
+
+
 
 module.exports = router;
