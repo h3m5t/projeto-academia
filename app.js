ad7d8fd61fbe7b7fea28efc79007051576bbbaa1
h3m5t <henriquee.trajanoo@gmail.com>
2025-12-09 18:08:57 -0300

    Resolvendo conflito no funcionario

--- app.js
+++ app.js
@@ -5,24 +5,15 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-
-var funcionarioRouter = require ('./routes/funcionario');
-var clienteRouter = require ('./routes/cliente');
-var pagamentoRouter = require ('./routes/pagamento');
-var horarioRouter = require ('./routes/horario');
-var cargoRouter = require ('./routes/cargo');
-var cors = require('cors');
+var funcionarioRouter = require('./routes/funcionario');
+var clienteRouter = require('./routes/cliente');
+var pagamentoRouter = require('./routes/pagamento');
+var horarioRouter = require('./routes/horario');
+var cargoRouter = require('./routes/cargo');
 
 var app = express();
-<<<<<<< HEAD
-app.use(express.json());
-=======
-app.use(cors()); // <--- 2. Liberamos o acesso
-app.use(express.json());
 
->>>>>>> a0252d654a34bd39721addb986b81ec31d54ad3e
-
-// view engine setup
+// Configuração da View Engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -32,32 +23,26 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(cors());
+app.use(cors()); // CORS ativado
 
-app.use('/funcionario',funcionarioRouter);
-app.use('/cliente',clienteRouter);
-app.use('/pagamento',pagamentoRouter);
-app.use('/horarios',horarioRouter);
-app.use('/cargo',cargoRouter);
+app.use('/funcionario', funcionarioRouter);
+app.use('/cliente', clienteRouter);
+app.use('/pagamento', pagamentoRouter);
+app.use('/horarios', horarioRouter);
+app.use('/cargo', cargoRouter);
 
 const moment = require('moment');
-app.locals.moment = moment; // Disponibiliza o moment para os templates EJS
+app.locals.moment = moment;
 
-
-
-
-// catch 404 and forward to error handler
+// Tratamento de erro 404
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// Manipulador de erros
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
